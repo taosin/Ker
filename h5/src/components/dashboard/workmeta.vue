481bea296a105e20f478a35ce665f5951e820e08
Nix <xinxintao@foxmail.com>
2019-10-15 18:50:27 +0800

    add layout-components

---
+++ h5/src/components/dashboard/workmeta.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="workmeta">
+      <div class="main">
+          <h1>拖拽或添加组件到此处</h1>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+    .workmeta{
+        height: 100%;
+        overflow-y: auto;
+        .main{
+            margin: 10px;
+            border: 1px dashed;
+            overflow: auto;
+            min-height: 85vh;
+        }
+    }
+</style>
--- h5/src/views/index.vue
+++ h5/src/views/index.vue
@@ -2,49 +2,77 @@
   <div class="index-view">
     <el-container>
       <el-aside width="300px">
-          <h1>Vue Code Maker</h1>
-          <sider></sider>
+        <h1>Vue Code Maker</h1>
+        <sider></sider>
       </el-aside>
       <el-container>
-        <el-header>Header</el-header>
-        <el-main>Main</el-main>
-        <!-- <el-footer>Footer</el-footer> -->
+        <el-header>
+          <top />
+        </el-header>
+        <el-main>
+          <el-container>
+            <div class="main-area">
+              <workMeta />
+            </div>
+            <el-aside class="aside-in-main"></el-aside>
+          </el-container>
+        </el-main>
+        <el-footer height="30px">Footer</el-footer>
       </el-container>
     </el-container>
   </div>
 </template>
 <script>
-import sider from '@/components/sider'
+import sider from "@/components/dashboard/sider";
+import top from "@/components/dashboard/header";
+import workMeta from "@/components/dashboard/workmeta";
 export default {
   data() {
     return {};
   },
-  components:{
-      sider
+  components: {
+    sider,
+    top,
+    workMeta
   }
 };
 </script>
 
 <style lang="less" scoped>
-.el-header,
-.el-footer {
-//   background: rgba(5, 64, 208, .8);
-    background: linear-gradient(to right,rgba(5, 64, 208, .5),rgba(5, 64, 208, .8),rgba(5, 64, 208, .5),rgba(5, 64, 208, .6));
+.el-header {
+  //   background: rgba(5, 64, 208, .8);
+  // background: linear-gradient(to right,rgba(5, 64, 208, .5),rgba(5, 64, 208, .8),rgba(5, 64, 208, .5),rgba(5, 64, 208, .6));
+  border-bottom: 1px solid;
   color: #333;
-  text-align: center;
   line-height: 60px;
 }
 .el-footer {
-    background: none;
+  border-top: 1px solid;
 }
 .el-container {
   height: 100%;
-  .el-aside{
-      border-right: 1px solid;
-      padding: 10px;
+  .el-aside {
+    border-right: 1px solid;
+    padding: 10px;
   }
-  .el-main{
-    //   border: 1px solid;
+  .el-main {
+    padding: 0px;
+    .aside-in-main {
+      border-left: 1px solid;
+      position: absolute;
+      right: 0;
+      top: 60px;
+      bottom: 30px;
+      width: 200px;
+    }
+  }
+  .main-area{
+    position: absolute;
+    left: 300px;
+    top: 60px;
+    right: 300px;
+    bottom: 30px;
+    overflow: hidden;
   }
 }
 </style>
