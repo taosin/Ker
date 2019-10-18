d133e088829f283242628c41809a03a74d21be73
Nix <xinxintao@foxmail.com>
2019-10-18 18:03:44 +0800

    add componentItem

---
+++ h5/src/components/dashboard/componentItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <div :is="item.name" v-bind="item.props">
+    <template v-for="(child,index) in item.children">
+      <componentItem :item="child" :key="index"></componentItem>
+    </template>
+    {{item.text}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "componentItem",
+  props: ["item"],
+  data() {
+    return {};
+  }
+};
+</script>
