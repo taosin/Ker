b2846dd9dfe3a96381aa70732fe6aa9c690c5b52
Nix <xinxintao@foxmail.com>
2019-10-18 10:14:09 +0800

    edit workmate.vue style

--- h5/src/components/dashboard/workmeta.vue
+++ h5/src/components/dashboard/workmeta.vue
@@ -1,26 +1,34 @@
 <template>
   <div class="workmeta">
-      <div class="main">
-          <h1>拖拽或添加组件到此处</h1>
+    <div class="main">
+      <div class="empty">
+        <h1>拖拽或添加组件到此处</h1>
       </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style lang="less" scoped>
-    .workmeta{
-        height: 100%;
-        overflow-y: auto;
-        .main{
-            margin: 10px;
-            border: 1px dashed;
-            overflow: auto;
-            min-height: 85vh;
-        }
+.workmeta {
+  height: 100%;
+  overflow-y: auto;
+  .main {
+    margin: 10px;
+    border: 1px dashed;
+    overflow: auto;
+    min-height: -webkit-fill-available;
+    .empty {
+      display: flex;
+      align-items: center;
+      width: 100%;
+      height: -webkit-fill-available;
+      justify-content: center;
+      background-color: #dedede;
     }
+  }
+}
 </style>
